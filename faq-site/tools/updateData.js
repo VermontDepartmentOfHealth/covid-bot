7f06389eb197acc391d1b458997d86bbc4bf08a4
Brian and Kyle <Brian.Nowak@vermont.gov>
2020-04-13 12:14:31 -0400

    #25 add synonym support fior mark.js from alterations Co-authored-by: Kyle Mitofsky <kyle.mitofsky@vermont.gov>

--- faq-site/tools/updateData.js
+++ faq-site/tools/updateData.js
@@ -13,19 +13,56 @@ async function updateData() {
         kbId: process.env.kbId
     })
 
+    updateKnowledgeBase(client)
+    updateAlterations(client)
+
+}
+
+async function updateKnowledgeBase(client) {
     let knowledgeBase = await client.knowledgeBase.download()
 
-    // write data
+    let contents = JSON.stringify(knowledgeBase, null, 4);
+
+    writeFile("_data/faqs.json", contents)
+}
+
+async function updateAlterations(client) {
+
+    let alterations = await client.alterations.get();
+
+    // etl mapping
+    let allEntries = alterations.wordAlterations.map(alts => {
+        let altWords = alts.alterations
+        let entries = altWords.map((el, i) => {
+            let nextIndex = i === altWords.length - 1 ? 0 : i + 1
+            let next = altWords[nextIndex]
+            let cur = altWords[i]
+
+            return [cur, next]
+        })
+
+        return entries
+    })
+    let flatEntries = [].concat(...allEntries)
+    let synonyms = Object.fromEntries(flatEntries)
+
+    let contents = "var synonyms = " + JSON.stringify(synonyms, null, 4);
+
+    writeFile("assets/synonyms.js", contents)
+}
+
+async function writeFile(path, contents) {
+
+
     let projectRoot = __dirname.replace(/tools$/, "");
-    let filePath = `${projectRoot}/_data/faqs.json`;
-    let content = JSON.stringify(knowledgeBase, null, 4);
+    let fullPath = `${projectRoot}/${path}`;
 
     try {
-        await fs.writeFile(filePath, content)
+        await fs.writeFile(fullPath, contents)
+
+        console.log(`Data has been written to ${path}`);
+
     } catch (error) {
         console.error(error)
     }
-
-    console.log(`Knowledge base data written to ${filePath}`);
-
 }
