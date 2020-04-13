f174cfd51f1e6267516e449ed19f3ad9dbc266d8
Brian and Kyle <Brian.Nowak@vermont.gov>
2020-04-13 11:12:28 -0400

    consume @ads-vdh/qnamaker-api on faq site Co-authored-by: Kyle Mitofsky <kyle.mitofsky@vermont.gov>

--- faq-site/tools/updateData.js
+++ faq-site/tools/updateData.js
@@ -1,25 +1,19 @@
 require('dotenv').config()
-const { promises: fs } = require("fs");
-const fetch = require('node-fetch');
+const {promises: fs} = require("fs");
+const qnaMakerApi = require('@ads-vdh/qnamaker-api');
 
 
 module.exports = updateData();
 
 async function updateData() {
 
-    // remove tools directory from current path
-    let url = `${process.env.Endpoint}/qnamaker/v4.0/knowledgebases/${process.env.kbId}/${process.env.environment}/qna`
-
-    
-    // get data
-    let resp = await fetch(url, {
-        method: 'get',
-        headers: {
-            "Ocp-Apim-Subscription-Key": process.env.OcpApimSubscriptionKey
-        },
+    let client = qnaMakerApi({
+        endpoint: process.env.Endpoint,
+        apiKey: process.env.OcpApimSubscriptionKey,
+        kbId: process.env.kbId
     })
-    let knowledgeBase = await resp.json();
 
+    let knowledgeBase = await client.knowledgeBase.download()
 
     // write data
     let projectRoot = __dirname.replace(/tools$/, "");
@@ -32,9 +26,6 @@ async function updateData() {
         console.error(error)
     }
 
-    console.log(`Knowlege base data written to ${filePath}`);
-
+    console.log(`Knowledge base data written to ${filePath}`);
 
 }
-// C:\Users\kylemit\Documents\Stash\covid-faq\_data\faqs.json
-// C:\Users\kylemit\Documents\Stash\covid-faq\tools\_data\faqs.js
