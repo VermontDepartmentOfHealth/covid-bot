6f93e53320861fa613f88d79fc47eb9de7a98d2a
Kyle Mitofsky <Kyle.Mitofsky@vermont.gov>
2020-04-17 13:00:17 -0400

    handle title casing edge cases

--- faq-site/tools/utilities.js
+++ faq-site/tools/utilities.js
@@ -38,10 +38,24 @@ function toTitleCase(str) {
     // capitalize first word, last word, and all words over two chars
     let words = str.split(" ")
     let cased = words.map((word, i) => {
+        // always capitalize first word
         if (i == 0) return toProperCase(word)
+            // always capitalize last word
         if (i == words.length - 1) return toProperCase(word)
-        if (word.length > 2) return toProperCase(word)
-        return word.toLowerCase()
+
+        // don't capitalize these words
+        let articles = ["a", "an", "the"]
+        let coordinatingConjunctions = ["for", "and", "nor", "but", "or", "yet", "so"]
+        let prepositions = ["at", "by", "to", "with", "in"]
+        let dontCapitalizeWords = articles.concat(coordinatingConjunctions).concat(prepositions)
+        let dontCapitalize = dontCapitalizeWords.some(w => w === word.toLowerCase())
+        if (dontCapitalize) return word.toLowerCase()
+
+        // lower case if less than or equal to 4 characters
+        if (word.length <= 4) return word.toLowerCase()
+
+        // otherwise uppercase everything else
+        return toProperCase(word)
     })
     return cased.join(" ")
 }
