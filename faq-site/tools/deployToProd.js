08ba6adf35c962ebf743ea9a4014cec4a79300ea
jmathias3 <john.mathias@vermont.gov>
2020-04-20 07:49:44 -0400

    #21 Automated deployment, minor refactors, simplified console messages

--- faq-site/tools/deployToProd.js
+++ faq-site/tools/deployToProd.js
@@ -1,8 +1,6 @@
 require('dotenv').config()
 const qnaMakerApi = require('@ads-vdh/qnamaker-api');
-const chalk = require("chalk");
-const successStatus = 204;
-
+const SUCCESS_STATUS = 204;
 
 module.exports = deployToProd();
 
@@ -14,48 +12,50 @@ async function deployToProd() {
         kbId: process.env.PROD_kbId
     })
 
+    let targetKbid = process.env.PROD_kbId;
+
     let sourceClient = qnaMakerApi({
         endpoint: process.env.Endpoint,
         apiKey: process.env.OcpApimSubscriptionKey,
         kbId: process.env.kbId
     })
 
-    console.log("\n" + chalk.blue.bold('Deploying Alterations...'))
-    let replaceAlterationsResultMsg = await deployAlterations(sourceClient,targetClient);
-    console.log(chalk.blue.bold(replaceAlterationsResultMsg))
+    console.log('Deploying Alterations...')
+    let replaceAlterationsWasSuccessful = await deployAlterations(sourceClient, targetClient);
+    let deployResultMsg = replaceAlterationsWasSuccessful ? "Alterations Deployed Successfully" : "Failed to Deploy alterations";
+    console.log(deployResultMsg)
 
-
-    console.log("\n" + chalk.blue.bold('Deploying Knowledgebase...'))  
-    let replaceKbWasSuccessful = await replaceKnowledgeBase(sourceClient,targetClient);
-    replaceKbResultMsg = replaceKbWasSuccessful ? 'KnowledgeBase Deployed Successfully' : 'Failed to Deploy KnowledgeBase';
-    console.log( chalk.blue.bold(replaceKbResultMsg))
-
+    console.log("\n" + 'Deploying Knowledgebase...')
+    let replaceKbWasSuccessful = await replaceKnowledgeBase(sourceClient, targetClient, targetKbid);
+    let replaceResultMsg = replaceKbWasSuccessful ? "KnowledgeBase Deployed Successfully" : "Failed to Deploy KnowledgeBase";
+    console.log(replaceResultMsg)
 
     if(replaceKbWasSuccessful){
-        console.log("\n" + chalk.blue.bold('Publishing Knowledgebase...'))
+        console.log("\n" + 'Publishing Knowledgebase...')
         let publishResponse = await targetClient.knowledgeBase.publish();
-        let resultMsg = publishResponse.status === successStatus ? 'Knowledgebase Published Successfully' : 'Failed to Publish Knowledgebase';
-        console.log(chalk.blue.bold(resultMsg))
+        let publishSuccessful = publishResponse.status === SUCCESS_STATUS;
+        let publishResultMsg = publishSuccessful ? "Knowledgebase Published Successfully" : "Failed to Publish Knowledgebase";
+        console.log(publishResultMsg)
     }
 
 }
 
-async function replaceKnowledgeBase(sourceClient,targetClient){
-    //download KB from test
-    let sourceKnowledgeBase = await sourceClient.knowledgeBase.download();
-    //call replace
-    let knowledgeBaseReplaceResonse = await targetClient.knowledgeBase.replace(targetClient.kbId, {qnaList: sourceKnowledgeBase.qnaDocuments});
-    //return bool to indicate if relace was successful
-    return knowledgeBaseReplaceResonse.status === successStatus;
-}
 
 async function deployAlterations(sourceClient,targetClient){
     //get alterations from test
-    let sourceAlterations = await sourceClient.alterations.get();  
+    let sourceAlterations = await sourceClient.alterations.get();
     //call replace
     let replaceAlterationsResponse = await targetClient.alterations.replace(sourceAlterations);
-    //return status message 
-    return replaceAlterationsResponse.status === successStatus ? 'Alterations Deployed Successfully' : 'Failed to Deploy alterations';
+    //return status message
+    return replaceAlterationsResponse.status === SUCCESS_STATUS;
 }
 
-
+async function replaceKnowledgeBase(sourceClient,targetClient, targetKbId){
+    //download KB from test
+    let sourceKnowledgeBase = await sourceClient.knowledgeBase.download();
+    let testKbid = targetClient.knowledgeBase.kbId;
+    //call replace 
+    let knowledgeBaseReplaceResonse = await targetClient.knowledgeBase.replace(targetKbId, {qnaList: sourceKnowledgeBase.qnaDocuments});
+    //return bool to indicate if relace was successful
+    return knowledgeBaseReplaceResonse.status === SUCCESS_STATUS;
+}
