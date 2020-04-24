afcb653b37af3385e240d4cc8d865f019c3ccf2c
Kyle Mitofsky <Kyle.Mitofsky@vermont.gov>
2020-04-24 08:59:25 -0400

    #46 open links in new window

--- faq-site/.eleventy.js
+++ faq-site/.eleventy.js
@@ -22,10 +22,46 @@ module.exports = function(eleventyConfig) {
 
     eleventyConfig.addFilter("slugify", title => utilities.slugify(title))
 
+    let md = customizeMarkdown();
 
-    let md = require("markdown-it")();
     eleventyConfig.addFilter("md", content => md.render(content))
-
+    eleventyConfig.setLibrary("md", md);
 
     return {};
 };
+
+function customizeMarkdown() {
+
+    let md = require("markdown-it")();
+
+    // Remember old renderer, if overridden, or proxy to default renderer
+    let defaultAnchorRender = md.renderer.rules.link_open || function(tokens, idx, options, env, self) {
+        return self.renderToken(tokens, idx, options);
+    };
+
+    md.renderer.rules.link_open = function(tokens, idx, options, env, self) {
+        // If you are sure other plugins can't add `target` - drop check below
+        let token = tokens[idx]
+
+        let setAttribute = function(token, attrName, attrValue, append) {
+            var index = token.attrIndex(attrName);
+
+            if (index < 0) {
+                // add new attribute
+                token.attrPush([attrName, attrValue]);
+            } else {
+                // update value of existing attr
+                token.attrs[index][1] = (append ? token.attrs[index][1] : "") + attrValue;
+            }
+        }
+
+        setAttribute(token, "target", "_blank")
+        setAttribute(token, "rel", "noopener")
+        setAttribute(token, "class", " external-link", true)
+
+        // pass token to default renderer.
+        return defaultAnchorRender(tokens, idx, options, env, self);
+    };
+
+    return md
+}
